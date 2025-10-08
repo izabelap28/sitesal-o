11566f03ee72194ed3682f5c9263ec2f2e216bb8
izabelap28 <izabela.parizotto@escola.pr.gov.br>
2025-10-08 09:08:41 -0300

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,14 @@
+const links = document.querySelectorAll('nav a');
+const secoes = document.querySelectorAll('.conteudo');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    links.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+    secoes.forEach(sec => sec.classList.remove('ativo'));
+    const alvo = document.querySelector(link.getAttribute('href'));
+    alvo.classList.add('ativo');
+  });
+});
 
